6a6cff42e3f40ee4aa9ad4025f171e4d0d1bab7a
AntoineDolley <99596888+AntoineDolley@users.noreply.github.com>
2025-03-14 17:16:31 +0100

    Update ast_parser.py

--- src/ast_parser.py
+++ src/ast_parser.py
@@ -1,11 +1,19 @@
 import clang.cindex
 
-def parse_source(source_path: str) -> clang.cindex.TranslationUnit:
+def parse_source(source_path: str, include_paths, library_paths) -> clang.cindex.TranslationUnit:
     """
     Parse le fichier source et retourne l'unité de traduction (AST)
     """
+    includes = ['-I' + path for path in include_paths]
+
+    libraries = ['-l' + lib for lib in library_paths]
+
+    args = includes + libraries
+
+    print(f"args{args}")
+
     index = clang.cindex.Index.create()
-    translation_unit = index.parse(source_path)
+    translation_unit = index.parse(source_path, args = args)
     return translation_unit
 
 def get_root_cursor(translation_unit: clang.cindex.TranslationUnit) -> clang.cindex.Cursor:
