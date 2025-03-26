c18234b87af688133d84ea04c821143807feb375
AntoineDolley <99596888+AntoineDolley@users.noreply.github.com>
2025-03-26 16:52:43 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+import logging
+from clang.cindex import CompilationDatabase
+from ast_graph_generator.lib import create_ast_graph_from_file_with_args
+from tqdm import tqdm
+
+
+
+def main():
+
+    directory = "/users/t0315611/Documents/codde-analyser-main"
+    os.chdir(directory)
+
+    source_file = "sep_test/Actions.cpp"
+    args = "-c -I./sep_test"
+    args = args.split()
+
+    filtered_args = []
+    for arg in args:
+        if not (arg.startswith('-o')): # or arg.startswith('-W')
+            filtered_args.append(arg)
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    create_ast_graph_from_file_with_args(source_file, filtered_args, export_dir)
+    
+
+if __name__ == "__main__":
+    main()
