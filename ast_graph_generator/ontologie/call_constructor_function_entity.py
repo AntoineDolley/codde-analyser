e71052641927c9e73fb953de5c57cbf8e320b072
AntoineDolley <99596888+AntoineDolley@users.noreply.github.com>
2025-03-28 17:10:25 +0100

    Create call_constructor_function_entity.py

---
+++ ast_graph_generator/ontologie/call_constructor_function_entity.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+import clang.cindex
+from .uses_type_ref_entity import TypeRefEntity
+from .ontologie_utils import get_function_signature, get_class_node
+
+@dataclass
+class ConstructorClassFunctionCallEntity(TypeRefEntity):
+    def __init__(self, node: clang.cindex.Cursor):
+        # On calcule la signature complète avant d'initialiser le reste
+        signature = get_function_signature(node)
+
+        class_node = get_class_node(node)
+
+        # Appel à l'initialisation de la classe parente pour récupérer les autres attributs
+        super().__init__(class_node) # Initialise correctement decl_file row et columns
+        # On remplace le nom par la signature complète
+        self.name = signature
+        self.namespace_position = self._build_namespace_position(node)
+        self.name = f"{self.decl_file}#{self.namespace_position}"
