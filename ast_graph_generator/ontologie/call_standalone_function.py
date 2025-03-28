a13dfbba34bfffc60053a44a93635f7bffe3d313
AntoineDolley <99596888+AntoineDolley@users.noreply.github.com>
2025-03-28 17:10:49 +0100

    Create call_standalone_function.py

---
+++ ast_graph_generator/ontologie/call_standalone_function.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+import clang.cindex
+from .entity import Entity
+from .ontologie_utils import get_function_signature
+
+@dataclass
+class StandaloneFunctionCallEntity(Entity):
+    def __init__(self, node: clang.cindex.Cursor):
+        # On calcule la signature complète avant d'initialiser le reste
+        signature = get_function_signature(node)
+
+        if node.referenced: 
+            new_node = node.referenced.get_definition()
+            if new_node is None:
+                try :
+                    new_node = node.referenced.get_declaration()
+                    if new_node is None:
+                        node = node
+                    else : 
+                        node = new_node
+                except : 
+                    #print(f"From func {node.spelling}")
+                    pass
+            else : 
+                node = new_node
+
+        # Appel à l'initialisation de la classe parente pour récupérer les autres attributs
+        super().__init__(node)
+        # On remplace le nom par la signature complète
+        self.name = signature
+        self.namespace_position = self._build_namespace_position(node)
+        self.name = f"{self.decl_file}#{self.namespace_position}"
